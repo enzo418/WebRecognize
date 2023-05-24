b0a0f0f8fd0202b07507cad7cddc32b566bbbe74
enzo418 <c.enzoaguirre@gmail.com>
2023-05-24 19:26:32 -0300

    ux: simplified configuration creation

--- src/pages/ConfigurationSelectionPage.tsx
+++ src/pages/ConfigurationSelectionPage.tsx
@@ -4,7 +4,13 @@ import {
     FormControl,
     Grid,
     InputLabel,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
     Stack,
+    SwipeableDrawer,
     Typography,
 } from '@mui/material';
 import React, { useEffect } from 'react';
@@ -12,6 +18,9 @@ import { configurationService } from '../services/api/Services';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import SelectConfiguration from '../components/SelectConfiguration';
 import { Key } from '../LocalStore';
+import AddIcon from '@mui/icons-material/Add';
+import UploadIcon from '@mui/icons-material/Upload';
+import { ContentCopy } from '@mui/icons-material';
 
 export default function ConfigurationSelection() {
     const onNewConfiguration = () => {
@@ -29,77 +38,104 @@ export default function ConfigurationSelection() {
         }
     };
 
+    const [showDrawer, setShowDrawer] = React.useState(false);
+
+    const toggleDrawer = () => {
+        setShowDrawer(!showDrawer);
+    };
+
     return (
-        <Grid
-            container
-            sx={{ padding: '10px', height: '100vh' }}
-            className="centered"
-            spacing={{ xs: 0, md: 2 }}>
+        <>
             <Grid
-                item
-                sx={{ padding: '10px 10px 30px 10px' }}
-                xs={12}
-                className="centered">
-                <Stack direction="column">
-                    <Typography
-                        variant="h5"
-                        gutterBottom
-                        sx={{ textAlign: 'center' }}>
-                        Select a configuration to open
-                    </Typography>
-                    <FormControl
-                        className="centered"
-                        sx={{ width: '100%', minWidth: '30vw' }}>
-                        <InputLabel id="select-db-configuration">
-                            Select
-                        </InputLabel>
-                        <SelectConfiguration
-                            sx={{ width: '100%' }}
-                            labelId="select-db-configuration"
-                            label="Configuration"
-                            onSelected={onChangeConfigurationSelected}
-                        />
-                    </FormControl>
-                </Stack>
-            </Grid>
-
-            <Grid container className="centered" spacing={{ xs: 0, md: 2 }}>
-                <Grid item xs={12} md={1}>
-                    <Typography variant="subtitle1" color="GrayText">
-                        ADD
-                    </Typography>
+                container
+                sx={{ padding: '10px', height: '100vh' }}
+                className="centered"
+                spacing={{ xs: 0, md: 2 }}>
+                <Grid
+                    item
+                    sx={{ padding: '10px 10px 30px 10px' }}
+                    xs={12}
+                    className="centered">
+                    <Stack direction="column">
+                        <Typography
+                            variant="h5"
+                            gutterBottom
+                            sx={{ textAlign: 'center' }}>
+                            Select a configuration to open
+                        </Typography>
+                        <FormControl
+                            className="centered"
+                            sx={{ width: '100%', minWidth: '30vw' }}>
+                            <InputLabel id="select-db-configuration">
+                                Select
+                            </InputLabel>
+                            <SelectConfiguration
+                                sx={{ width: '100%' }}
+                                labelId="select-db-configuration"
+                                label="Configuration"
+                                onSelected={onChangeConfigurationSelected}
+                            />
+                        </FormControl>
+                    </Stack>
                 </Grid>
 
-                <Grid item xs={12} md={2}>
-                    <Button
-                        variant="outlined"
-                        component={RouterLink}
-                        fullWidth
-                        to="/configuration_file">
-                        From file
-                    </Button>
-                </Grid>
-
-                <Grid item xs={12} md={4}>
-                    <Button
-                        variant="outlined"
-                        component={RouterLink}
-                        sx={{ textAlign: 'center' }}
-                        fullWidth
-                        to="/configuration-clone">
-                        Clone from existing configuration
-                    </Button>
-                </Grid>
-
-                <Grid item xs={12} md={1}>
-                    <Button
-                        variant="outlined"
-                        fullWidth
-                        onClick={onNewConfiguration}>
-                        New
-                    </Button>
+                <Grid container className="centered" spacing={{ xs: 0, md: 2 }}>
+                    <Grid item xs={12} md={1}>
+                        <Button
+                            variant="outlined"
+                            fullWidth
+                            onClick={toggleDrawer}>
+                            New
+                        </Button>
+                    </Grid>
                 </Grid>
             </Grid>
-        </Grid>
+            <SwipeableDrawer
+                anchor={'bottom'}
+                open={showDrawer}
+                onClose={toggleDrawer}
+                onOpen={toggleDrawer}>
+                <Box>
+                    <List>
+                        <ListItem
+                            onClick={() => {
+                                toggleDrawer();
+                                onNewConfiguration();
+                            }}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <AddIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="New configuration" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem
+                            onClick={() => {
+                                toggleDrawer();
+                                navigate('/configuration_file');
+                            }}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <UploadIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Upload" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem
+                            onClick={() => {
+                                toggleDrawer();
+                                navigate('/configuration-clone');
+                            }}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <ContentCopy />
+                                </ListItemIcon>
+                                <ListItemText primary="Clone from existing configuration" />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </Box>
+            </SwipeableDrawer>
+        </>
     );
 }
